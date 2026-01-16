828d83a792fedd1541d732b3dfaf1540c9a2e718
akshat-jain-bits <2025aa05010@wilp.bits-pilani.ac.in>
2026-01-16 16:05:38 +0530

    Changed estimators to 100 from 300

--- models/random_forest.py
+++ models/random_forest.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 def get_model():
     return RandomForestClassifier(
-        n_estimators=300,
+        n_estimators=100,
         random_state=42,
         class_weight="balanced",
         n_jobs=-1
